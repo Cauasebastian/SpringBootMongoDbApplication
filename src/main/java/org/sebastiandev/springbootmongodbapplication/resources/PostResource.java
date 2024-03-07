3ff0ff7fcc6621dc47dac6972f792e8e58e6c132
Cauã Sebastian <cauasebastian2207@gmail.com>
2024-03-06 23:07:45 -0300

    Getting a post by ID and by FindAll

---
+++ src/main/java/org/sebastiandev/springbootmongodbapplication/resources/PostResource.java
@@ -0,0 +1,29 @@
+package org.sebastiandev.springbootmongodbapplication.resources;
+
+
+import org.sebastiandev.springbootmongodbapplication.domain.Post;
+import org.sebastiandev.springbootmongodbapplication.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value="/posts")
+public class PostResource {
+
+	@Autowired
+	private PostService service;
+
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<Post> findById(@PathVariable String id) {
+		Post obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findAll() {
+		List<Post> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+}
---
+++ src/main/java/org/sebastiandev/springbootmongodbapplication/services/PostService.java
@@ -0,0 +1,27 @@
+package org.sebastiandev.springbootmongodbapplication.services;
+
+import org.sebastiandev.springbootmongodbapplication.domain.Post;
+import org.sebastiandev.springbootmongodbapplication.domain.User;
+import org.sebastiandev.springbootmongodbapplication.dto.UserDTO;
+import org.sebastiandev.springbootmongodbapplication.reposistory.PostRepository;
+import org.sebastiandev.springbootmongodbapplication.reposistory.UserRepository;
+import org.sebastiandev.springbootmongodbapplication.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PostService {
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        return repo.findById(id)
+                .orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+    public List<Post> findAll() {
+        return repo.findAll();
+    }
+
+}
