058d184316ec8d6efd62cab0a1edc6fb1aafc4ce
Cauã Sebastian <cauasebastian2207@gmail.com>
2024-03-07 02:12:25 -0300

    Query with multiple criteria

--- src/main/java/org/sebastiandev/springbootmongodbapplication/services/PostService.java
+++ src/main/java/org/sebastiandev/springbootmongodbapplication/services/PostService.java
@@ -9,6 +9,7 @@ import org.sebastiandev.springbootmongodbapplication.services.exception.ObjectNo
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -25,5 +26,9 @@ public class PostService {
     }
     public List<Post> findByTitle(String text) {
         return repo.searchTitle(text);
+    }
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+        return repo.fullSearch(text, minDate, maxDate);
     }
 }
